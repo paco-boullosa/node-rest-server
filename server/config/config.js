4d2df063d3b33a3086a0fa04040d12320902daf6
Fran <francisco.boullosa@gmail.com>
2019-10-31 12:28:15 +0100

    Versión alfa

---
+++ server/config/config.js
@@ -0,0 +1,5 @@
+// ==========================
+// PUERTO
+// ==========================
+// si el PORT está configurado por el servidor (como en  Heroku) se usa ese valor. En otro caso se inicializa a 3000
+process.env.PORT = process.env.PORT || 3000;
